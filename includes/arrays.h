6085f586c7944f36f74900bdb9a706a7d6a8f883
H-b-IO-T-O-H <56133777+H-b-IO-T-O-H@users.noreply.github.com>
2020-03-18 03:59:12 +0300

    Update arrays.h
    change code style

--- includes/arrays.h
+++ includes/arrays.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>
 #include <sys/sysinfo.h>
 
 #define MAX_SIZE 300000000
@@ -18,30 +17,30 @@ typedef struct	s_info {
 	int size;
 	int pos;
 	int test_mode;
-}				t_info;
+}		t_info;
 
 typedef struct	s_data {
 	pthread_mutex_t	mutex;
-	t_info			info;
-}				t_data;
+	t_info		info;
+}		t_data;
 
 //single_thread_app
-int		*ft_fill_array(int size, int fill_flag);
-int		compare_arrays(const int *array_A,
-						const int *array_B, const int size);
+int	*ft_fill_array(int size, int fill_flag);
+int	compare_arrays(const int *array_A,
+		       const int *array_B, const int size);
 //multi_thread_app
 void	*cmp_arrs(void *arg);
 void	*single_thrd_fill(void *arg);
 void	fill_arrays_via_treads(pthread_t *trds, int opt_trds_cnt);
-int		create_threads_and_cmp(int *array_A, int *array_B,
-								int size, int fill_flag);
-int		compare_arrays_via_threads(const int *array_A, const int *array_B,
-									  pthread_t *trds, int opt_trds_cnt);
+int	create_threads_and_cmp(int *array_A, int *array_B,	
+			       int size, int fill_flag);
+int	compare_arrays_via_threads(const int *array_A, const int *array_B,
+				   pthread_t *trds, int opt_trds_cnt);
 //utils
-int		*ft_create_array(int size);
+int	*ft_create_array(int size);
 void	ft_print_array(const int *arr, const int size);
 void	free_all(int *array_A, int *array_B,
-				int *distribution, pthread_mutex_t *mutex);
+		 int *distribution, pthread_mutex_t *mutex);
 int 	request_data(int argc, char **argv, int *size, int *flag_fill);
 
 #endif //DZ_2_LISTS_H
